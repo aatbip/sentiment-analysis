23e56e8ecce7775f5fa0cdfcf345d090d1ddea96
Ishantiwari14 <ishantiwari14@gmail.com>
2023-07-18 13:01:10 +0545

    added pie chart

---
+++ src/components/MovieDetail.jsx
@@ -0,0 +1,4 @@
+import { Box, Typography, Stack } from '@mui/material'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
--- src/pages/ReviewPage.jsx
+++ src/pages/ReviewPage.jsx
@@ -4,12 +4,17 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import MovieList from '../components/MovieList';
 import ReviewList from '../components/ReviewList';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js"
+import {Pie} from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const ReviewPage = () => {
 
   const [reviews, setReviews] = useState();
   const [movieInfo, setMovieInfo] = useState(null);
-
+  const [chartData, setChartData] = useState({});
+  
   let positive = 0;
   let negative = 0;
 
@@ -41,6 +46,21 @@ const ReviewPage = () => {
   }, [reviews])
 
   useEffect(() => {
+    if (reviews) {
+      
+      const chartData = {
+        labels: ["Positive", "Negative"],
+        datasets: [
+          {
+            data: [positive, negative],
+            backgroundColor: ["#2ecc71", "#e74c3c"]
+          },
+        ]
+      }
+      setChartData(chartData)
+    }
+  }, [reviews])
+  useEffect(() => {
     const fetchMovieInfo = async () => {
       try {
         const {data} = await axios.get(`/movies/${id}/`);
@@ -61,10 +81,16 @@ const ReviewPage = () => {
     <Box sx={{flex: '1 0 60%'}}>
 
       <Typography variant="h4">Overall Sentiment: {positive > negative ? "POSITIVE" : "NEGATIVE"}</Typography>
+      <Box sx={{ marginTop: '2rem' , width: '20vh', height: '20vh'}}>
+          {Object.keys(chartData).length > 0 && (
+            <Pie data={chartData} />
+            )}
+
+        </Box>
       <Stack direction="column" rowGap="2rem">
         {
           reviews?.map((el, key) => {
-            return <ReviewList  key={key} id={el.id} critic_name={el.critic_name} content={el.content} review_date={el.review_date} sentimentPred={el.sentiment_pred} prob_neg={el.prob_neg} prob_pos={el.prob_pos}/>
+            return <ReviewList  key={key} id={el.id} critic_name={el.critic_name} content={el.content} review_date={el.review_date} sentimentPred={el.sentiment_pred} rating={el.rating} prob_neg={el.prob_neg} prob_pos={el.prob_pos}/>
           })
         }
       </Stack>
@@ -75,7 +101,7 @@ const ReviewPage = () => {
         <img
           src={movieInfo.medium_poster_url}
           alt={movieInfo.title}
-          style={{width: '40vh', height: '40vh'}}
+          style={{width: '70%', height: '50vh', objectFit: 'fit'}}
         />
         <Typography variant='h3'>{movieInfo.title}</Typography>
         <Typography variant='body1'>Date of Release: {movieInfo.date_released}</Typography>
